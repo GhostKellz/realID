3709b52cc717e3c354d5259e2d594deb2b823b17
GhostKellz <ckelley@ghostkellz.sh>
2025-06-26 21:33:23 -0400

    fixes for zwallet integration

---
+++ .zig-cache/o/5881860a50f7d403ffc37097fea076fc/dependencies.zig
@@ -0,0 +1,12 @@
+pub const packages = struct {
+    pub const @"zcrypto-0.0.0-rgQAIzgEBADjnAEqmjRN2TmhRa-O8lScNIa0xpkZS59t" = struct {
+        pub const build_root = "/home/chris/.cache/zig/p/zcrypto-0.0.0-rgQAIzgEBADjnAEqmjRN2TmhRa-O8lScNIa0xpkZS59t";
+        pub const build_zig = @import("zcrypto-0.0.0-rgQAIzgEBADjnAEqmjRN2TmhRa-O8lScNIa0xpkZS59t");
+        pub const deps: []const struct { []const u8, []const u8 } = &.{
+        };
+    };
+};
+
+pub const root_deps: []const struct { []const u8, []const u8 } = &.{
+    .{ "zcrypto", "zcrypto-0.0.0-rgQAIzgEBADjnAEqmjRN2TmhRa-O8lScNIa0xpkZS59t" },
+};
